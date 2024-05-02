6b3074a971bbc415dd3e52fb64f03b54cbe7624c
GonzaNahuel <gonzalonahuel010@gmail.com>
2024-05-02 11:26:08 -0300

    avances home

--- script.js
+++ script.js
@@ -14,3 +14,50 @@ function showSlides() {
 }
 
 showSlides();
+
+
+
+const cardData = [
+  {
+      title: 'Card 1',
+      text: 'Contenido de la primera card.',
+      link: 'https://www.example.com'
+  },
+  {
+      title: 'Card 2',
+      text: 'Contenido de la segunda card.',
+      link: 'https://www.example.com'
+  },
+  {
+    title: 'Card 3',
+    text: 'Contenido de la segunda card.',
+    link: 'https://www.example.com'
+}
+
+  // Agrega más objetos de datos aquí
+];
+
+// Función para crear una card
+function createCard(title, text, link) {
+  const cardDiv = document.createElement('div');
+  cardDiv.classList.add('card');
+
+  const cardContent = `
+      <img src="recursos/libro3.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${text}</p>
+          <a href="${link}" class="btn btn-primary">Ir a algún lugar</a>
+      </div>
+  `;
+
+  cardDiv.innerHTML = cardContent;
+  return cardDiv;
+}
+
+// Agrega las cards al contenedor
+const cardsContainer = document.getElementById('cards-container');
+cardData.forEach(card => {
+  const newCard = createCard(card.title, card.text, card.link);
+  cardsContainer.appendChild(newCard);
+});
